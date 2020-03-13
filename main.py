fa501bef032bcfb8be50a0756ffb5fd5ddaa49a3
artyrkurin <kurinartyr@gmail.com>
2020-03-13 19:07:28 +0300

    add user to company
    Signed-off-by: artyrkurin <kurinartyr@gmail.com>

--- main.py
+++ main.py
@@ -21,23 +21,24 @@ def contact():
 
 @app.route("/company")
 def company():
-    items = collection_companies.find()
+    items = collection_companies.find({})
     docs = list(items)
     result = []
     for doc in docs:
-        item = {'name': doc['name'], 'email': doc['email']}
-
-        result.append(item)
+        item = {'name': doc['name'], 'email': doc['email'], 'Сотрудники': [doc['_id']]}
+        if doc['Сотрудники']:
+            for data in doc['Сотрудники']:
+                emp = collection_contacts.find_one({'_id': data['_id']})
+                if emp:
+                    item['staff'].append({'name': emp['name']})
+            result.append(item)
+        print(result)
     return render_template('company.html', items=result)
 
 
 @app.route("/remove", methods=['GET', 'POST'])
-def remove_id():
+def remove_contact():
     key = request.values.get("_id")
-    cmp = collection_companies.find_one({'_id': key})
-    if cmp:
-        collection_companies.remove({"_id": ObjectId(key)})
-        return redirect("/company")
     collection_contacts.remove({"_id": ObjectId(key)})
     return redirect("/")
 
@@ -52,38 +53,65 @@ def add_new_user():
     return redirect('/')
 
 
-@app.route('/add_new_company', methods=['POST'])
+@app.route('/company/add_new_company', methods=['POST'])
 def add_new_company():
     companies = collection_companies
     name_user = request.values.get('name')
     email = request.values.get('email')
-    companies.insert({"name": name_user, "email": email, "Сотрудники": {}})
+    companies.insert({"name": name_user, "email": email, "Сотрудники": []})
     return redirect('/company')
 
 
-@app.route("/add")
-def add_user_to_company():
-    key = request.values.get("_id")
-    print(collection_companies.find_one({'Сотрудники': {'_id': key}}))
-    return redirect('/company')
-
-
-@app.route("/update")
+@app.route("/company/update")
 def update():
     id = request.values.get("_id")
     task = collection_contacts.find({"_id": ObjectId(id)})
     return render_template('result.html', tasks=task)
 
 
-@app.route("/action3", methods=['POST'])
+@app.route("/company/action3", methods=['POST'])
 def update_contact():
     # Updating a Task with various references
     id = request.values.get("_id")
     name = request.values.get('name_user')
     email = request.values.get('email')
     position = request.values.get('position')
-    collection_contacts.update({"_id": ObjectId(id)}, {'$set': {"name_user": name, "email": email, "position": position}})
+    collection_contacts.update({"_id": ObjectId(id)},
+                               {'$set': {"name_user": name, "email": email, "position": position}})
     return redirect("/")
+
+
+@app.route("/company/add")
+def add_user_to_company():
+    # key = request.values.get("_id")
+    # name = request.values.get('name')
+    # email = request.values.get('email')
+    # find_in_contacts = collection_contacts.find_one({'Сотрудники': {'_id': key}})
+    # find_in_company = collection_companies.find_one({'Сотрудники': {'_id': key}})
+    # if find_in_company:
+    #     print('В наличии')
+    #     redirect('/company')
+    # else:
+    #     collection_companies.update({"_id": ObjectId(key)},
+    #                                 {'$set': {"name": name, "email": email, 'Сотрудники': {'_id': find_in_contacts}}})
+    return redirect('/company')
+
+
+@app.route("/update", methods=['POST'])
+def update_company():
+    # Updating a Task with various references
+    id = request.values.get("_id")
+    name = request.values.get('name')
+    email = request.values.get('email')
+    collection_companies.update({"_id": ObjectId(id)}, {'$set': {"name": name, "email": email}})
+    return redirect("/")
+
+
+@app.route("/company/removing", methods=['GET', 'POST'])
+def remove_company():
+    key = request.values.get('_id')
+    collection_companies.remove({"_id": ObjectId(key)})
+    return redirect("/company")
 
 
 if __name__ == '__main__':
